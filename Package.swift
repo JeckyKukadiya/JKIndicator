95615d94a3b8f92dfe709208bf66b9dba1dcdbc7
“Jecky” <jackykukadiya@gmail.com>
2024-10-14 17:44:38 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "JKIndicator",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "JKIndicator",
+            targets: ["JKIndicator"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "JKIndicator"
+//            path: "Source"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
